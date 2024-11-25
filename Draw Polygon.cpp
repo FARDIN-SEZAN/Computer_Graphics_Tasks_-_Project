0a980a0764c8faa60a37c27cf044ae7873e7aaf6
FARDIN-AL-SEZAN <140816473+FARDIN-SEZAN@users.noreply.github.com>
2024-11-25 18:27:38 +0600

    Add files via upload

---
+++ Draw Polygon.cpp
@@ -0,0 +1,46 @@
+
+/*
+* GL02Primitive.cpp: Vertex, Primitive and Color
+* Draw Simple 2D colored Shapes: quad, triangle and polygon.
+*/
+#include <windows.h>  // for MS Windows
+#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+
+/* Initialize OpenGL Graphics */
+void initGL() {
+	// Set "clearing" or background color
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // Black and opaque
+}
+
+/* Handler for window-repaint event. Call back when the window first appears and
+whenever the window needs to be re-painted. */
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);   // Clear the color buffer with current clearing color
+
+
+	glBegin(GL_POLYGON);            // These vertices form a closed polygon
+	glColor3f(1.0f, 0.0f,0.0f ); // Yellow
+	glVertex2f(-6.2043897391283,4.218603953316);
+	glVertex2f(-2.9915673412021,4.2019572051402);
+	glVertex2f(-2.2358049740216,1.2188599320399);
+	glVertex2f(-6.9468347077683,1.2188599320399);
+
+	glEnd();
+
+	glFlush();  // Render now
+}
+
+/* Main function: GLUT runs as a console application starting at main()  */
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);          // Initialize GLUT
+	glutCreateWindow("Vertex, Primitive & Color");  // Create window with the given title
+	glutInitWindowSize(320, 320);   // Set the window's initial width & height
+	glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+	 gluOrtho2D(-15,15,-15,15);
+	glutDisplayFunc(display);       // Register callback handler for window re-paint event
+	initGL();                       // Our own OpenGL initialization
+	glutMainLoop();                 // Enter the event-processing loop
+	return 0;
+}
+
+
